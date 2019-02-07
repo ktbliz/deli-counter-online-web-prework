b27af54b3d6b926248a28e10cd7301cd02d16605
Katie Blizzard <kblizzard11@gmail.com>
2019-02-07 01:52:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,8 +14,12 @@ end
 
 
 def take_a_number(array, name)
-  puts "Welcome, #{name}. You are number #{array.size + 1} in line."
-
+  if array.size < 1 
+    puts "Wecome, #{name}. You are number 1 in line."
+  else 
+    puts "Welcome, #{name}. You are number #{array.size + 1} in line."
+  end
+  array << name 
 end 
 
 
