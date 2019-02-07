7a2d7206cc4c7de76e0f65beb98be2f5770eda1a
Katie Blizzard <kblizzard11@gmail.com>
2019-02-07 01:30:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.size >= 1 
     print "The line is currently: "
     for x in array 
-      prints "#{array.index(x) + 1}. #{x}"  
+      print "#{array.index(x) + 1}. #{x}"  
     end
   else 
     puts "The line is currently empty."
