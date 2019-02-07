00c131452f2b336d1d0d17eb81e64b300b7b12f4
Katie Blizzard <kblizzard11@gmail.com>
2019-02-07 02:01:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,10 @@ def take_a_number(array, name)
 end 
 
 
-def now_serving()
-  
+def now_serving(array)
+  if array.size < 1 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{array[0]}."
+    array.shift
 end 
